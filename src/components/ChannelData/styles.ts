704f9f57e1992e6f76bdb2e1e28f310637d18915
Octavio Arruda <octavio@maxiprod.com.br>
2020-08-01 20:00:29 -0300

    hotfix for AlternateEmail button in ChannelData component

--- src/components/ChannelData/styles.ts
+++ src/components/ChannelData/styles.ts
@@ -1,74 +1,61 @@
 import styled from 'styled-components';
 import { AlternateEmail } from 'styled-icons/material';
- 
+
 export const Container = styled.div`
-    grid-area: CD; 
-    
-    display: flex;
-    flex-direction: column;
-    
-    justify-content: space-between;
-
-    background-color: var(--primary);
+  grid-area: CD;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: var(--primary);
 `;
+
 export const Messages = styled.div`
-    padding: 20px 0;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-
-    max-height: calc(100vh - 46px - 68px);
-
-    overflow-y: scroll;
-
-    ::-webkit-scrollbar { 
-        width: 8px;
-    }
-
-    ::-webkit-scrollbar-thumb { 
-        background-color: var(--tertiary);
-        border-radius: 4px;
-    }
-
-    ::-webkit-scrollbar-track { 
-        background-color: var(--secondary);
-    }
+  padding: 20px 0;
+  display: flex;
+  flex-direction: column;
+  max-height: calc(100vh - 46px - 68px);
+  overflow-y: scroll;
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: var(--tertiary);
+    border-radius: 4px;
+  }
+  ::-webkit-scrollbar-track {
+    background-color: var(--secondary);
+  }
 `;
 
 export const InputWrapper = styled.div`
-    width: 100%;
-
-    padding: 0 16px;    
+  margin: 0 16px 23px;
+  position: relative;
 `;
 
 export const Input = styled.input`
-    width: 100%;
-    height: 44px;
-    
-    padding: 0 10px 0 57px;
-    border-radius: 7px;
+  width: 100%;
+  height: 44px;
+  padding: 0 10px 0 57px;
+  border-radius: 7px;
+  color: var(--white);
+  background-color: var(--chat-input);
+  position: relative;
 
-    color: var(--white);
-    background-color: var(--chat-input);
+  &::placeholder {
+    color: var(--gray);
+  }
 
-    position: relative;
-
-    &::placeholder {
-        color: var(--gray);
-    }
-
-    ~ svg { 
-        position: relative;
-        top: -50%;
-        left: 14px;
-        transition: 180ms ease-in-out;
-    }
+  ~ svg {
+    position: absolute;
+    top: 50%;
+    left: 28.5px;
+    transform: translate(-50%, -50%);
+    transition: 180ms ease-in-out;
+  }
 `;
 
 export const InputIcon = styled(AlternateEmail)`
-    width: 24px;
-    height: 24px;
-    
-    color: var(--gray);
+  width: 24px;
+  height: 24px;
+  color: var(--gray);
 `;
